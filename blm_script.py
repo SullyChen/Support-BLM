147020f6fec7027084660fe551d4c1facba70f91
Nathan Tsai <natesigh@gmail.com>
2020-06-02 17:39:31 -0700

    use list comprehension for links, use sleep instead of wait

--- blm_script.py
+++ blm_script.py
@@ -3,26 +3,23 @@ import random
 from youtube_search import YoutubeSearch
 import time
 
-#wait an exact time in seconds
-def wait(wait_time):
-    target_time = time.clock() + wait_time
-    while time.clock() < target_time:
-        pass
-
 max_results = 30 #number of random BLM videos to query from YouTube
 results = YoutubeSearch('black lives matter -fox', max_results=max_results).to_dict() #query YouTube for videos, exclude Fox news
 
-#generate array of youtube links
-links = []
-for l in results:
-    links.append("https://youtube.com" + l["link"])
+# print(results)
 
-print(links)
+#generate array of youtube links
+links = ["https://youtube.com" + l["link"] for l in results]
+# print(links)
 
 #main loop
 while True:
-    webbrowser.open("https://youtu.be/bCgLa25fDHM") #open BLM-ad video
-    wait(3386) #wait until the video finishes
+    print("starting BLM video...")
+
+    webbrowser.open("https://youtu.be/bCgLa25fDHM", new=1) #open BLM-ad video in new window
+    time.sleep(3386); #wait until the video finishes
+
+
     for i in range(0, random.randint(3, 5)): #pick a random number of videos to open
-        webbrowser.open(links[random.randrange(max_results)]) #open the video
-        wait(60) #wait a minute to open the next one
+        webbrowser.open(links[random.randrange(max_results)]) #open a random blm video
+        time.sleep(60) #wait a minute to open the next one
